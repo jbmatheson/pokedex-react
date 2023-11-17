4c74540dacc23febee4c42d62adedd1800443064
jimmatheson <jim@rmg.io>
2023-11-17 00:01:32 -0500

    github page push

--- docs/static/js/3.1d003881.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpokedex=self.webpackChunkpokedex||[]).push([[3],{4378:(e,s,t)=>{t.r(s),t.d(s,{default:()=>d});var c=t(821),n=t(7363),u=t(2298);const d=e=>{const{message:s}=e,{t:t}=(0,n.$G)(["common"]),[d,r]=(0,c.useState)(!0);return(0,c.useEffect)((()=>{setTimeout((()=>{r(!1)}),3e3)}),[r]),(0,u.jsx)(u.Fragment,{children:d&&(0,u.jsx)("div",{children:(0,u.jsx)("div",{})})})}}}]);
-//# sourceMappingURL=3.1d003881.chunk.js.map
